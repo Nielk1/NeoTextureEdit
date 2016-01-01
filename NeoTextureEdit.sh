351803a47900ca2c34e7b5ddde438bcf26b0001c
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-01-01 04:17:06 +0000

    fix launcher script

--- NeoTextureEdit.sh
+++ NeoTextureEdit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-java -Djava.library.path=./lib/ -cp ./lib/lwjgl.jar:./lib/lwjgl_util.jar:./lib/NeoTextureEdit.jar com.mystictri.neotextureedit.TextureEditor $@
+LWJGL_VERS=2.9.1
+exec java -Djava.library.path=lib/lwjgl-$LWJGL_VERS/ -cp lib/lwjgl-$LWJGL_VERS/jar/lwjgl.jar:lib/lwjgl-$LWJGL_VERS/jar/lwjgl_util.jar:NeoTextureEdit.jar com.mystictri.neotextureedit.TextureEditor $@
 
