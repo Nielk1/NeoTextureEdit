3e4580209a48ff6b98fb230e32f0a68fe9ff84c6
Nielk1 <development@nielk1.com>
2018-08-15 03:53:18 -0500

    no message

---
+++ NeoTextureEdit.bat
@@ -0,0 +1,2 @@
+SET LWJGL_VERS=2.9.2
+java -Djava.library.path=lib/lwjgl-%LWJGL_VERS%/ -cp lib/lwjgl-%LWJGL_VERS%/jar/lwjgl.jar;lib/lwjgl-%LWJGL_VERS%/jar/lwjgl_util.jar;NeoTextureEdit.jar com.mystictri.neotextureedit.TextureEditor %*
