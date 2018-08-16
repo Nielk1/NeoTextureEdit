3e777ccd5a4b6bbe2ad7e749e12571a845fa6fc2
Nielk1 <development@nielk1.com>
2018-08-16 11:38:54 -0500

    Added the missing shader files and some eclipse file adjustments

---
+++ src/com/mystictri/neotextureedit/AbstractShader.java
@@ -0,0 +1,240 @@
+package com.mystictri.neotextureedit;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.ARBVertexShader;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.Util;
+import org.lwjgl.util.glu.GLU;
+
+import engine.base.Logger;
+import engine.base.Utils;
+import engine.graphics.synthesis.texture.Channel;
+import engine.parameters.LocalParameterManager;
+
+abstract class AbstractShader {
+	final int TEXTURE_RESX = 256;
+	final int TEXTURE_RESY = 256;
+	
+	int getUniformLocation(String loc_name, int programID) {
+		byte[] bs = loc_name.getBytes();
+		ByteBuffer b = Utils.allocByteBuffer(bs.length+1);
+		b.put(bs); b.put((byte)0);
+		b.flip();
+		int ret = ARBShaderObjects.glGetUniformLocationARB(programID, b);
+		if (ret == -1) {
+			Logger.logWarning(this, "    WARNING: Shader: "+programID+": could not get uniform location " + loc_name);
+		}
+		Util.checkGLError();
+		return ret;
+	}
+	
+	abstract void loadShaderLocations();
+	
+	protected abstract int GetProgramID();
+	
+	public void bindTangentAttribute(String name) {
+		ByteBuffer nameb = Utils.allocByteBuffer(name.length()+1);
+		nameb.put(name.getBytes()); nameb.put((byte)0);
+		nameb.flip();
+		GL20.glBindAttribLocation(GetProgramID(), 1, nameb); 
+	}
+
+	public abstract void UpdateDiffuse(Channel _updateDiffuse);
+
+	public abstract void UpdateNormal(Channel _updateNormal);
+
+	public abstract void UpdateSpecWeight(Channel _updateSpecWeight);
+
+	public abstract void UpdateHeightmap(Channel _updateHeightmap);
+
+	public abstract void render1();
+	public abstract void render2();
+
+	public abstract void updateCamera(float eye_x, float eye_y, float eye_z, FloatBuffer m_CamONB);
+	
+	protected abstract void initGLState();
+	
+	public int getShaderProgram(String vcode, String fcode) {
+		ByteBuffer vc = ByteBuffer.allocateDirect(vcode.length());
+		vc.put(vcode.getBytes()); vc.flip();
+		ByteBuffer fc = ByteBuffer.allocateDirect(fcode.length());
+		fc.put(fcode.getBytes()); fc.flip();
+		return getShaderProgram(vc, fc);
+	}
+	
+	/**
+	 * Loads a vertex and fragment shader pair, compiles them and links them to a ProgramObjectARB
+	 * @param name
+	 * @return the program object ID
+	 */
+	public int getShaderProgram(ByteBuffer vcode, ByteBuffer fcode) {
+		int progID = -1;
+
+		int vid = ARBShaderObjects.glCreateShaderObjectARB(ARBVertexShader.GL_VERTEX_SHADER_ARB);
+		int fid = ARBShaderObjects.glCreateShaderObjectARB(ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
+
+		ARBShaderObjects.glShaderSourceARB(vid, vcode);
+		ARBShaderObjects.glCompileShaderARB(vid);
+		printLogInfo(vid, "Compile Vertex Shader");
+
+		ARBShaderObjects.glShaderSourceARB(fid, fcode);
+		ARBShaderObjects.glCompileShaderARB(fid);
+		printLogInfo(fid, "Compile Fragment Shader");
+		
+		progID = ARBShaderObjects.glCreateProgramObjectARB();
+		ARBShaderObjects.glAttachObjectARB(progID, vid);
+		ARBShaderObjects.glAttachObjectARB(progID, fid);
+		
+		ARBShaderObjects.glLinkProgramARB(progID);
+		printLogInfo(progID, "Link");
+		ARBShaderObjects.glValidateProgramARB(progID);
+		printLogInfo(progID, "Validate");
+		
+		return progID;
+	}
+	
+	
+	
+	
+	
+	
+	ByteBuffer bbuf = Utils.allocByteBuffer(TEXTURE_RESX * TEXTURE_RESY * 4);
+
+	//!!TODO: it is inefficient to do the texture creation over a temporary BufferedImage as a procedural texture channel alrady operates
+	//        on a float buffer that could be directly send to OpenGL for conversion (or at least converted directly in the convertImageData
+	//        function below.
+	public int create2dTexture(int color) {
+		int id = genTexID();
+		
+		IntBuffer ibuf = bbuf.asIntBuffer();
+		for (int i = 0; i < ibuf.capacity(); i++) ibuf.put(i, color);
+
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		int targetFormat = GL11.GL_RGBA;
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, targetFormat, TEXTURE_RESX, TEXTURE_RESY, 0, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf.asIntBuffer());
+		GLU.gluBuild2DMipmaps(GL11.GL_TEXTURE_2D, targetFormat, TEXTURE_RESX, TEXTURE_RESY, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf);
+
+		return id;
+	}
+	
+	public synchronized void update2dTexture_ConstanctColor(int color, int id) {
+		IntBuffer ibuf = bbuf.asIntBuffer();
+		for (int i = 0; i < ibuf.capacity(); i++) ibuf.put(i, color);
+
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		int targetFormat = GL11.GL_RGBA;
+		GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, 0, targetFormat, TEXTURE_RESX, TEXTURE_RESY, 0, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf.asIntBuffer());
+		GLU.gluBuild2DMipmaps(GL11.GL_TEXTURE_2D, targetFormat, TEXTURE_RESX, TEXTURE_RESY, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf);
+	}
+
+	
+	// here are the same performance problems as in create2dTexture
+	public synchronized void update2dTexture(BufferedImage img, int id) {
+		if (img.getWidth() != TEXTURE_RESX || img.getHeight() != TEXTURE_RESY) {
+			Logger.logError(this, "TextureResolution does not match image resolution for update.");
+			return;
+		}
+		convertImageData(bbuf.asIntBuffer(), img);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		//int targetFormat = (img.getColorModel().hasAlpha()) ? GL11.GL_RGBA : GL11.GL_RGB;
+		int targetFormat = GL11.GL_RGBA;
+		GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, 0, targetFormat, TEXTURE_RESX, TEXTURE_RESY, 0, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf.asIntBuffer());
+		GLU.gluBuild2DMipmaps(GL11.GL_TEXTURE_2D, targetFormat, TEXTURE_RESX, TEXTURE_RESY, GL11.GL_RGBA,
+				GL11.GL_UNSIGNED_BYTE, bbuf);
+	}
+	
+	
+	
+	
+	// !!TODO: this is a slow conversion operation because each byte is touched by hand...
+	public void convertImageData(IntBuffer result, BufferedImage img) {
+		int xres = img.getWidth();
+		int yres = img.getHeight();
+		int[] data = new int[xres * yres];
+		img.getRGB(0, 0, xres, yres, data, 0, xres);
+
+		for (int i = 0; i < xres * yres; i++) {
+			int A = ((data[i] >> 24) & 0xFF);
+			int R = ((data[i] >> 16) & 0xFF);
+			int G = ((data[i] >> 8) & 0xFF);
+			int B = ((data[i] >> 0) & 0xFF);
+			data[i] = (R << 24) | (G << 16) | (B << 8) | (A << 0);
+		}
+
+		result.put(data);
+		result.rewind();
+	}
+	
+	
+	ByteBuffer loadShaderCodeFromFile(String filename) {
+		byte[] code = null;
+
+		try {
+			InputStream fs = OpenGLPreviewPanel.class.getResourceAsStream(filename);
+			if (fs == null) {
+				System.err.println("Error opening file "+filename);
+				return null;
+			}
+			fs.read(code = new byte[fs.available()]);
+			fs.close();
+		} catch (IOException e) {
+			System.out.println(e);
+			return null;
+		}
+
+		ByteBuffer shaderCode = Utils.allocByteBuffer(code.length);
+		
+		shaderCode.put(code);
+		shaderCode.flip();
+
+		return shaderCode;
+	}
+	
+	public int genTexID() {
+		IntBuffer i = Utils.allocIntBuffer(1);
+		GL11.glGenTextures(i);
+		return i.get(0);
+	}
+	
+	private void printLogInfo(int obj, String name) {
+		IntBuffer iVal = Utils.allocIntBuffer(1);
+		ARBShaderObjects.glGetObjectParameterARB(obj, ARBShaderObjects.GL_OBJECT_INFO_LOG_LENGTH_ARB, iVal);
+
+		int length = iVal.get();
+		if (length > 1) {
+			System.out.println();
+			ByteBuffer infoLog = Utils.allocByteBuffer(length);
+			iVal.flip();
+			ARBShaderObjects.glGetInfoLogARB(obj, iVal, infoLog);
+			byte[] infoBytes = new byte[length];
+			infoLog.get(infoBytes);
+			String out = new String(infoBytes);
+			System.out.println("Info log: " + name + "\n" + out);
+		}
+
+		Util.checkGLError();
+	}
+
+	public abstract LocalParameterManager GetParamaters();
+	
+	
+}
---
+++ src/com/mystictri/neotextureedit/ShaderBasic.java
@@ -0,0 +1,243 @@
+package com.mystictri.neotextureedit;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.util.glu.GLU;
+
+import com.mystictri.neotextureedit.OpenGLTextureRenderCanvas.GLPreviewParameters;
+
+import engine.parameters.AbstractParam;
+import engine.parameters.EnumParam;
+import engine.parameters.FloatParam;
+import engine.parameters.IntParam;
+import engine.parameters.LocalParameterManager;
+import engine.parameters.ParamChangeListener;
+import engine.graphics.synthesis.texture.Channel;
+
+class ShaderBasic extends AbstractShader {
+
+	int texidDiffuse = 0;
+	int texidNormal = 0;
+	int texidSpecWeight = 0;
+	int texidHeightmap = 0;
+	
+	int previewProgram = 0;
+	
+	int u_WS_EyePos_loc;
+	int u_CameraONB_loc;
+	int _2dDiffuseMap_loc;
+	int _2dNormalMap_loc;
+	int _2dSpecWeightMap_loc;
+	int _2dHeightMap_loc;
+	
+	int u_SpecularPower_loc;
+	int u_POM_Strength_loc;
+	int u_TexScale_loc;
+	int u_Ambient_loc;
+	
+	static class BasicShaderPreviewParameters extends LocalParameterManager {
+		public FloatParam specularPower = CreateLocalFloatParam("Spec. Power", 20.0f, 0.f, 200.0f);
+		public FloatParam pomStrength = CreateLocalFloatParam("POM Strength", 0.25f, 0.f, 1.0f).setDefaultIncrement(0.125f);
+		public FloatParam ambient = CreateLocalFloatParam("Ambient", 0.5f, 0.f, 1.0f).setDefaultIncrement(0.125f);
+		public IntParam texScaleU = CreateLocalIntParam("TexScale U", 1, 1, 8);
+		public IntParam texScaleV = CreateLocalIntParam("TexScale V", 1, 1, 8);
+	}
+	
+	BasicShaderPreviewParameters params = new BasicShaderPreviewParameters();
+	
+	@Override
+	public LocalParameterManager GetParamaters() {
+		return (LocalParameterManager)params;
+	}
+	
+	// very basic preview shader (does compute the camera transform extra to provide world space coordinates for the fragment shader)
+	String previewVertexShader = 		
+		"uniform vec3 u_WS_EyePos; "+
+		"uniform mat3 u_CameraONB; "+
+		"attribute vec3 a_OS_tangent; "+
+		
+		"varying vec3 v_WS_vertex; "+
+		"varying vec3 v_WS_normal; "+
+		"varying vec3 v_WS_tangent; "+
+		
+		"void main(void) { "+
+		"	gl_TexCoord[0] = gl_MultiTexCoord0; "+
+		
+		"	v_WS_vertex = vec3(gl_ModelViewMatrix * gl_Vertex); "+
+		"	v_WS_normal = vec3(gl_NormalMatrix * gl_Normal); "+
+		"	v_WS_tangent = vec3(gl_NormalMatrix * a_OS_tangent); "+
+		
+		"	gl_Position = gl_ProjectionMatrix * vec4((v_WS_vertex - u_WS_EyePos) * u_CameraONB, 1); "+
+		"}";
+	
+	// very basic preview shader
+	String previewFragmentShader = 
+		"uniform vec3 u_WS_EyePos; " +
+
+		"uniform sampler2D _2dTex0; " +
+		"uniform sampler2D _2dNormalMap; " +
+		"uniform sampler2D _2dSpecWeightMap; " +
+		"uniform sampler2D _2dHeightMap; " +
+
+		"varying vec3 v_WS_vertex; " +
+		"varying vec3 v_WS_normal; " +
+		"varying vec3 v_WS_tangent; " +
+		"" +
+		"uniform float u_SpecularPower; " +
+		"uniform float u_POM_Strength; " +
+		"uniform vec2 u_TexScale; " +
+		"uniform float u_Ambient; " +
+		"" +
+
+		"void main(void){ " +
+		"	vec3 lightPos = vec3(20.0, 60.0, 40.0); " +
+
+		"	vec3 lightDir = normalize(lightPos - v_WS_vertex); " +
+
+		"	mat3 onb; " +
+		"	onb[2] = normalize(v_WS_normal); " +
+		"	onb[0] = normalize(v_WS_tangent - dot(onb[2], v_WS_tangent)*onb[2]); " +
+		"	onb[1] = cross(onb[2], onb[0]); " +
+
+		"	vec3 rayDir = v_WS_vertex - u_WS_EyePos; " +
+		"	vec3 eyeDir = normalize(rayDir * onb); " +
+
+		"	vec3 pos = vec3(gl_TexCoord[0].xy * u_TexScale, 1.0); " +
+
+		"   rayDir = eyeDir; " +
+		"   if (rayDir.z > 0.0) rayDir.z = -rayDir.z; " +
+		"	if (u_POM_Strength > 0.0) {" +
+		"     rayDir.z /= 0.25*u_POM_Strength; " +
+		"     rayDir.x *= 0.5; " +
+		"     rayDir.y *= 0.5; " +
+		"     for (int i = 0; i < 512; i++) { " +
+		"     	  if (pos.z < 1.0-(texture2D(_2dHeightMap, pos.xy).r)) break; " +
+		"   	  pos += rayDir*0.001; " +
+		"     } " +
+		"   } " +
+
+		"	vec3 normal = onb * normalize(texture2D(_2dNormalMap, pos.xy).rgb * 2.0 - 1.0); " +
+
+		"	vec3 refl = reflect(lightDir, normal); " +
+		"	float SpecWeightyness = texture2D(_2dSpecWeightMap, pos.xy).x;" +
+		"	float spec = pow(max(dot(refl, eyeDir), 0.0), u_SpecularPower)*0.5*SpecWeightyness; " +
+		"	float diff = dot(lightDir, normal); " +
+		"	if (diff < 0.0) diff = 0.0; " +
+		"	diff = min(diff+u_Ambient, 1.0); " +
+		"	vec4 color = vec4(texture2D(_2dTex0, pos.xy).xyz, 0.0) * diff + spec; " +
+
+		"	gl_FragColor = color; " +
+		"}";
+	
+	@Override
+	void loadShaderLocations() {
+		u_WS_EyePos_loc = getUniformLocation("u_WS_EyePos", previewProgram);
+		u_CameraONB_loc = getUniformLocation("u_CameraONB", previewProgram);
+		
+		_2dDiffuseMap_loc = getUniformLocation("_2dTex0", previewProgram);
+		_2dNormalMap_loc  = getUniformLocation("_2dNormalMap", previewProgram);
+		_2dSpecWeightMap_loc  = getUniformLocation("_2dSpecWeightMap", previewProgram);
+		_2dHeightMap_loc  = getUniformLocation("_2dHeightMap", previewProgram);
+		
+		u_SpecularPower_loc = getUniformLocation("u_SpecularPower", previewProgram);
+		u_POM_Strength_loc = getUniformLocation("u_POM_Strength", previewProgram);
+		u_TexScale_loc = getUniformLocation("u_TexScale", previewProgram);
+		u_Ambient_loc = getUniformLocation("u_Ambient", previewProgram);
+		
+		bindTangentAttribute("a_OS_tangent");
+	}
+	
+	@Override
+	public void UpdateDiffuse(Channel _updateDiffuse) {
+		if (_updateDiffuse != null) update2dTexture(ChannelUtils.createAndComputeImage(_updateDiffuse, TEXTURE_RESX, TEXTURE_RESY, null, 0), texidDiffuse);
+		else update2dTexture_ConstanctColor(0x7F7FFFFF, texidDiffuse);
+	}
+		
+	@Override
+	public void UpdateNormal(Channel _updateNormal) {
+		if (_updateNormal != null) update2dTexture(ChannelUtils.createAndComputeImage(_updateNormal, TEXTURE_RESX, TEXTURE_RESY, null, 0), texidNormal);
+		else update2dTexture_ConstanctColor(0x7F7FFFFF, texidNormal);
+	}
+	
+	@Override
+	public void UpdateSpecWeight(Channel _updateSpecWeight) {
+		if (_updateSpecWeight != null) update2dTexture(ChannelUtils.createAndComputeImage(_updateSpecWeight, TEXTURE_RESX, TEXTURE_RESY, null, 0), texidSpecWeight);
+		else update2dTexture_ConstanctColor(0xFFFFFFFF, texidSpecWeight);
+	}
+		
+	@Override
+	public void UpdateHeightmap(Channel _updateHeightmap) {
+		if (_updateHeightmap != null) update2dTexture(ChannelUtils.createAndComputeImage(_updateHeightmap, TEXTURE_RESX, TEXTURE_RESY, null, 0), texidHeightmap);
+		else update2dTexture_ConstanctColor(0xFFFFFFFF, texidHeightmap);
+	}
+
+	@Override
+	protected int GetProgramID() {
+		return previewProgram;
+	}
+	
+	@Override
+	public void render1() {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
+		
+		
+		ARBShaderObjects.glUseProgramObjectARB(previewProgram);
+	}
+	
+	@Override
+	public void render2() {
+		GL13.glActiveTexture(GL13.GL_TEXTURE0+0);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texidDiffuse);
+		ARBShaderObjects.glUniform1iARB(_2dDiffuseMap_loc, 0);			
+		
+		GL13.glActiveTexture(GL13.GL_TEXTURE0+1);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texidNormal);
+		ARBShaderObjects.glUniform1iARB(_2dNormalMap_loc, 1);			
+		
+		GL13.glActiveTexture(GL13.GL_TEXTURE0+2);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texidSpecWeight);
+		ARBShaderObjects.glUniform1iARB(_2dSpecWeightMap_loc, 2);			
+
+		GL13.glActiveTexture(GL13.GL_TEXTURE0+3);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texidHeightmap);
+		ARBShaderObjects.glUniform1iARB(_2dHeightMap_loc, 3);			
+
+		ARBShaderObjects.glUniform1fARB(u_SpecularPower_loc, params.specularPower.get());
+		ARBShaderObjects.glUniform1fARB(u_POM_Strength_loc, params.pomStrength.get());
+		ARBShaderObjects.glUniform2fARB(u_TexScale_loc, params.texScaleU.get(), params.texScaleV.get());
+		ARBShaderObjects.glUniform1fARB(u_Ambient_loc, params.ambient.get());
+	}
+	
+	@Override
+	public void updateCamera(float eye_x, float eye_y, float eye_z, FloatBuffer m_CamONB) {
+		ARBShaderObjects.glUniform3fARB(u_WS_EyePos_loc, eye_x, eye_y, eye_z);
+		ARBShaderObjects.glUniformMatrix3ARB(u_CameraONB_loc, false, m_CamONB);
+	}
+	
+	@Override
+	protected void initGLState() {
+		previewProgram = getShaderProgram(previewVertexShader, previewFragmentShader);
+		
+		// TODO: consider running this every render call with a boolean to know if it's already run
+		// this would allow the shader to do "per execute" actions if needed
+		loadShaderLocations();
+
+		// hmm: temp initialization here
+		// create the empty textures (later they are updated from the channels)
+		texidDiffuse = create2dTexture(0x7F7FFFFF);
+		texidNormal = create2dTexture(0x7F7FFFFF);
+		texidSpecWeight = create2dTexture(0xFFFFFFFF);
+		texidHeightmap = create2dTexture(0xFFFFFFFF);
+		
+	}
+}
